d3ab73cb7c56449f2392a8986cbdff4c47889eaf
Flora Calbris <flora.calbris@gmail.com>
2018-09-05 12:18:49 +0200

    controller

--- src/main/java/com/web/controller/HelloController.java
+++ src/main/java/com/web/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.web.controller;
 
+import java.text.MessageFormat;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,10 +18,10 @@ public class HelloController {
 		return "Ok!";
 	}
 
-	@GetMapping("/")
+	@GetMapping("/healthcheck/{user}")
 	@ResponseBody
-	public String variable() {
-		return ("Hi");
+	public String variable(@PathVariable String user) {
+		return MessageFormat.format("Hi {0}", user);
 	}
 
 }
